165de04eb90230c12500d65177ebc717c9af7b67
John Gerrard Holland <john_holland1@brown.edu>
2023-06-30 14:21:43 -0400

    refactor: update wrap_to_use_state to allow for overrides

--- src/autora/workflow_/executor.py
+++ src/autora/workflow_/executor.py
@@ -15,7 +15,9 @@ from autora.workflow_.state import BaseState, Delta, S
 
 
 def wrap_to_use_state(f):
-    """
+    """Decorator to make target `f` into a function on a `State` and `**kwargs`.
+
+    This wrapper makes it easier to pass arguments to a function from a State.
 
     Args:
         f:
@@ -24,19 +26,24 @@ def wrap_to_use_state(f):
 
     Examples:
         >>> from dataclasses import dataclass
+
+        The `State` it operates on needs to have the metadata described in the state module:
         >>> @dataclass
         ... class S(BaseState):
         ...     conditions: list[int] = field(metadata={"delta": "replace"})
 
+        As before, we indicate the inputs required by the parameter names.
+        The output must be a `Delta` object.
+        >>> from autora.workflow_ import Delta
         >>> @wrap_to_use_state
-        ... def function(conditions):
+        ... def experimentalist(conditions):
         ...     new_conditions = [c + 10 for c in conditions]
         ...     return Delta(conditions=new_conditions)
 
-        >>> function(S(conditions=[1,2,3,4]))
+        >>> experimentalist(S(conditions=[1,2,3,4]))
         S(conditions=[11, 12, 13, 14])
 
-        >>> function(S(conditions=[101,102,103,104]))
+        >>> experimentalist(S(conditions=[101,102,103,104]))
         S(conditions=[111, 112, 113, 114])
 
         >>> from autora.variable import VariableCollection, Variable
@@ -44,11 +51,11 @@ def wrap_to_use_state(f):
         >>> from sklearn.linear_model import LinearRegression
 
         >>> @wrap_to_use_state
-        ... def function(experimental_data: pd.DataFrame, variables: VariableCollection):
+        ... def theorist(experimental_data: pd.DataFrame, variables: VariableCollection, **kwargs):
         ...     ivs = [v.name for v in variables.independent_variables]
         ...     dvs = [v.name for v in variables.dependent_variables]
         ...     X, y = experimental_data[ivs], experimental_data[dvs]
-        ...     new_model = LinearRegression(fit_intercept=True).fit(X, y)
+        ...     new_model = LinearRegression(fit_intercept=True).set_params(**kwargs).fit(X, y)
         ...     return Delta(model=new_model)
 
         >>> @dataclass(frozen=True)
@@ -62,19 +69,26 @@ def wrap_to_use_state(f):
         ...                                  dependent_variables=[Variable("y")]),
         ...     experimental_data=pd.DataFrame({"x": [0,1,2,3,4], "y": [2,3,4,5,6]})
         ... )
-        >>> t_prime = function(t)
+        >>> t_prime = theorist(t)
         >>> t_prime.model.coef_, t_prime.model.intercept_
         (array([[1.]]), array([2.]))
+
+        Arguments from the state can be overridden by passing them in as keyword arguments (
+        kwargs):
+        >>> theorist(t, fit_intercept=False).model.intercept_
+        0.0
     """
-    parameters = inspect.signature(f).parameters
+    parameters_ = inspect.signature(f).parameters
 
     @wraps(f)
-    # TODO: how do we handle the params here?
-    def _f(state: S, params: Optional[dict] = None) -> S:
+    def _f(state_: S, /, **kwargs) -> S:
         # Convert the dataclass to a dict of parameters
-        arguments = dict((p, getattr(state, p)) for p in parameters)
+        arguments_from_state = dict(
+            (p, getattr(state_, p)) for p in parameters_ if p != "kwargs"
+        )
+        arguments = dict(arguments_from_state, **kwargs)
         delta = f(**arguments)
-        new_state = state + delta
+        new_state = state_ + delta
         return new_state
 
     return _f

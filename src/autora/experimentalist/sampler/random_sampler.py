03f639af9f3a2f2ab25ed0756f05c449e2cc1ff4
John Gerrard Holland <john_holland1@brown.edu>
2023-07-12 12:41:49 -0400

    refactor: update random_sampler to use singledispatch

--- src/autora/experimentalist/sampler/random_sampler.py
+++ src/autora/experimentalist/sampler/random_sampler.py
@@ -1,14 +1,32 @@
 import random
+from functools import singledispatch
 from typing import Iterable, Optional, Sequence, Union
 
+import numpy as np
 import pandas as pd
 
-from autora.state.delta import Result, wrap_to_use_state
+from autora.state.delta import Result, State, wrap_to_use_state
 from autora.utils.deprecation import deprecated_alias
 
 
-def random_sample_from_conditions_iterable(
-    conditions: Union[Iterable, Sequence], num_samples: int = 1
+@singledispatch
+def random_sample(s, **kwargs):
+    """Function to create a sequence of conditions randomly sampled from independent variables."""
+    raise NotImplementedError(
+        "random_sample doesn't have an implementation for %s (type=%s)" % (s, type(s))
+    )
+
+
+@random_sample.register(State)
+def random_sample_on_state(s: State, **kwargs) -> State:
+    return wrap_to_use_state(random_sample_from_conditions)(s, **kwargs)
+
+
+@random_sample.register(list)
+@random_sample.register(range)
+@random_sample.register(filter)
+def random_sample_on_iterable_conditions(
+    conditions: Union[Sequence], num_samples: int = 1
 ):
     """
     Uniform random sampling without replacement from a pool of conditions.
@@ -21,15 +39,15 @@ def random_sample_from_conditions_iterable(
     Examples:
         From a range:
         >>> random.seed(1)
-        >>> random_sample_from_conditions_iterable(range(100), num_samples=5)
+        >>> random_sample(range(100), num_samples=5)
         [53, 37, 65, 51, 4]
 
         >>> random.seed(1)
-        >>> random_sample_from_conditions_iterable([1,2,3,4,5,6,7,8,9,10], num_samples=5)
+        >>> random_sample([1,2,3,4,5,6,7,8,9,10], num_samples=5)
         [7, 9, 10, 8, 6]
 
         >>> random.seed(1)
-        >>> random_sample_from_conditions_iterable(
+        >>> random_sample(
         ...     filter(lambda x: (x % 3 == 0) & (x % 5 == 0), range(1_000)),
         ...     num_samples=5
         ... )
@@ -44,13 +62,14 @@ def random_sample_from_conditions_iterable(
     return samples
 
 
-random_sampler = deprecated_alias(
-    random_sample_from_conditions_iterable, "random_sampler"
-)
+random_sampler = deprecated_alias(random_sample, "random_sampler")
 
 
+@random_sample.register(pd.DataFrame)
+@random_sample.register(np.ndarray)
+@random_sample.register(np.recarray)
 def random_sample_from_conditions(
-    conditions,
+    conditions: Union[pd.DataFrame, np.ndarray, np.recarray],
     num_samples: int = 1,
     random_state: Optional[int] = None,
     replace: bool = False,
@@ -70,7 +89,7 @@ def random_sample_from_conditions(
         From a pd.DataFrame:
         >>> import pandas as pd
         >>> random.seed(1)
-        >>> random_sample_from_conditions(
+        >>> random_sample(
         ...     pd.DataFrame({"x": range(100, 200)}), num_samples=5, random_state=180)
         {'conditions':       x
         67  167
@@ -85,6 +104,3 @@ def random_sample_from_conditions(
             conditions, random_state=random_state, n=num_samples, replace=replace
         )
     )
-
-
-random_sample = wrap_to_use_state(random_sample_from_conditions)

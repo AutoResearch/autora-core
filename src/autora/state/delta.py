7dec7295eeff89ef5b2b153b2c43a3b7d9312ea8
John Gerrard Holland <john_holland1@brown.edu>
2023-07-12 10:04:23 -0400

    docs: update docstring

--- src/autora/state/delta.py
+++ src/autora/state/delta.py
@@ -1,4 +1,4 @@
-"""Classes to represent cycle state $S$ as $S_n = S_{0} + \\sum_{i=1}^n \\Delta S_{i}$"""
+"""Classes to represent cycle state $S$ as $S_n = S_{0} + \\sum_{i=1}^n \\Delta S_{i}$."""
 from __future__ import annotations
 
 import dataclasses

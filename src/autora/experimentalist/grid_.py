5e70c7bfc603ff7b9416f21dc93c638e0a572833
John Gerrard Holland <john_holland1@brown.edu>
2023-07-18 14:32:51 -0400

    refactor: simplify grid_pool to make fundamental function the one with the shorter name, and add _on_state (placeholder extension) to the version which works on the state object

--- src/autora/experimentalist/grid_.py
+++ src/autora/experimentalist/grid_.py
@@ -1,28 +1,12 @@
 """Tools to make grids of experimental conditions."""
-from functools import singledispatch
 from itertools import product
-from typing import Sequence
 
 import pandas as pd
 
 from autora.state.delta import Result, State, wrap_to_use_state
-from autora.variable import Variable, VariableCollection
+from autora.variable import VariableCollection
 
 
-@singledispatch
-def grid_pool(s, **___):
-    """
-    Create an exhaustive pool of conditions.
-
-    Depending on the type of the first argument, this will return a different result-type.
-
-    """
-    raise NotImplementedError(
-        "grid_pool doesn't have an implementation for %s (type=%s)" % (s, type(s))
-    )
-
-
-@grid_pool.register(State)
 def grid_pool_on_state(s: State, **kwargs) -> State:
     """
     Create an exhaustive pool of conditions.
@@ -34,11 +18,12 @@ def grid_pool_on_state(s: State, **kwargs) -> State:
 
     Examples:
         >>> from autora.state.bundled import StandardState
+        >>> from autora.variable import Variable, VariableCollection
         >>> s = StandardState(variables=VariableCollection(
         ...     independent_variables=[
         ...         Variable(name="x1", allowed_values=[1, 2]),
         ...         Variable(name="x2", allowed_values=[3, 4])]))
-        >>> grid_pool(s)  # doctest: +ELLIPSIS +NORMALIZE_WHITESPACE
+        >>> grid_pool_on_state(s)  # doctest: +ELLIPSIS +NORMALIZE_WHITESPACE
         StandardState(..., conditions=
            x1  x2
         0   1   3
@@ -48,70 +33,10 @@ def grid_pool_on_state(s: State, **kwargs) -> State:
 
     """
 
-    return wrap_to_use_state(grid_pool_on_variables)(s, **kwargs)
+    return wrap_to_use_state(grid_pool)(s, **kwargs)
 
 
-@grid_pool.register(list)
-@grid_pool.register(tuple)
-def grid_pool_on_ivs(ivs: Sequence[Variable]) -> pd.DataFrame:
-    """
-    Create an exhaustive pool of conditions.
-
-    Args:
-        ivs: Variable objects, each of which has an attribute `allowed_values`
-            containing a sequence of values.
-
-    Returns: A pd.DataFrame with the exhaustive pool of allowed values
-
-    Examples:
-        >>> grid_pool([Variable("x", allowed_values=[1,2])])
-           x
-        0  1
-        1  2
-
-        >>> grid_pool([Variable("x", allowed_values=[1,2]),
-        ...            Variable("y", allowed_values=["a","b"])])
-           x  y
-        0  1  a
-        1  1  b
-        2  2  a
-        3  2  b
-
-        >>> grid_pool([Variable("x", allowed_values=[1,2]),
-        ...            Variable("y", allowed_values=["a","b"]),
-        ...            Variable("z", allowed_values=[3.0,4.0])])
-           x  y    z
-        0  1  a  3.0
-        1  1  a  4.0
-        2  1  b  3.0
-        3  1  b  4.0
-        4  2  a  3.0
-        5  2  a  4.0
-        6  2  b  3.0
-        7  2  b  4.0
-
-
-    """
-    # Get allowed values for each IV
-    l_iv_values = []
-    l_iv_names = []
-    for iv in ivs:
-        assert iv.allowed_values is not None, (
-            f"grid_pool only supports independent variables with discrete allowed values, "
-            f"but allowed_values is None on {iv=} "
-        )
-        l_iv_values.append(iv.allowed_values)
-        l_iv_names.append(iv.name)
-
-    # Return Cartesian product of all IV values
-    pool = product(*l_iv_values)
-    result = pd.DataFrame(pool, columns=l_iv_names)
-
-    return result
-
-
-@grid_pool.register(VariableCollection)
-def grid_pool_on_variables(variables: VariableCollection) -> Result:
+def grid_pool(variables: VariableCollection) -> Result:
     """Creates exhaustive pool of conditions given a definition of variables with allowed_values.
 
     Args:
@@ -190,5 +115,20 @@ def grid_pool_on_variables(variables: VariableCollection) -> Result:
         [2222 rows x 3 columns]
 
     """
-    conditions = grid_pool_on_ivs(variables.independent_variables)
+    ivs = variables.independent_variables
+    # Get allowed values for each IV
+    l_iv_values = []
+    l_iv_names = []
+    for iv in ivs:
+        assert iv.allowed_values is not None, (
+            f"grid_pool only supports independent variables with discrete allowed values, "
+            f"but allowed_values is None on {iv=} "
+        )
+        l_iv_values.append(iv.allowed_values)
+        l_iv_names.append(iv.name)
+
+    # Return Cartesian product of all IV values
+    pool = product(*l_iv_values)
+    conditions = pd.DataFrame(pool, columns=l_iv_names)
+
     return Result(conditions=conditions)

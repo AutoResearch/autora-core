6035eeac4982b69d96ec1180ca6ab5831b33966b
John Gerrard Holland <john_holland1@brown.edu>
2023-07-24 09:36:55 -0400

    exploration: add some function naming options

--- src/autora/experimentalist/random_/__init__.py
+++ src/autora/experimentalist/random_/__init__.py
@@ -1,1 +1,8 @@
 """Tools to make randomly sampled experimental conditions."""
+
+# Option 1
+from .pool import _state as random_pool_s
+from .pool import _state as random_pool_state
+from .pool import _state as random_pool_t
+from .pool import _state as random_pool_task
+from .pool import _state as random_pool_wf
--- src/autora/experimentalist/random_/pool.py
+++ src/autora/experimentalist/random_/pool.py
@@ -1,14 +1,14 @@
-"""Tools to make randomly sampled experimental conditions."""
-from typing import Optional, Union
+from dataclasses import dataclass, field
+from typing import Optional
 
 import numpy as np
 import pandas as pd
 
 from autora.state.delta import Result, State, wrap_to_use_state
-from autora.variable import ValueType, VariableCollection
+from autora.variable import ValueType, Variable, VariableCollection
 
 
-def random_pool_on_state(
+def _state(
     s: State,
     num_samples: int = 5,
     random_state: Optional[int] = None,
@@ -47,7 +47,7 @@ def random_pool_on_state(
         ... ]))
 
         ... we get some of those values back when running the experimentalist:
-        >>> random_pool_on_state(s, random_state=1).conditions
+        >>> _state(s, random_state=1).conditions
            x
         0  4
         1  5
@@ -62,7 +62,7 @@ def random_pool_on_state(
         ... ]))
 
         ... we get a sample of the range back when running the experimentalist:
-        >>> random_pool_on_state(t, random_state=1).conditions
+        >>> _state(t, random_state=1).conditions
                   x
         0  0.118216
         1  4.504637
@@ -73,7 +73,7 @@ def random_pool_on_state(
 
 
         The allowed_values or value_range must be specified:
-        >>> random_pool_on_state(
+        >>> _state(
         ...     S(variables=VariableCollection(independent_variables=[Variable(name="x")])))
         Traceback (most recent call last):
         ...
@@ -85,7 +85,7 @@ def random_pool_on_state(
         ...         Variable(name="x1", allowed_values=range(1, 5)),
         ...         Variable(name="x2", allowed_values=range(1, 500)),
         ... ]))
-        >>> random_pool_on_state(t, num_samples=10, replace=True, random_state=1).conditions
+        >>> _state(t, num_samples=10, replace=True, random_state=1).conditions
            x1   x2
         0   2  434
         1   3  212
@@ -99,7 +99,7 @@ def random_pool_on_state(
         9   2   14
 
         If any of the variables have unspecified allowed_values, we get an error:
-        >>> random_pool_on_state(S(
+        >>> _state(S(
         ...     variables=VariableCollection(independent_variables=[
         ...         Variable(name="x1", allowed_values=[1, 2]),
         ...         Variable(name="x2"),
@@ -116,7 +116,7 @@ def random_pool_on_state(
         ...         Variable(name="y", allowed_values=[3, 4]),
         ...         Variable(name="z", allowed_values=np.linspace(20, 30, 11)),
         ... ]))
-        >>> random_pool_on_state(u, random_state=1).conditions
+        >>> _state(u, random_state=1).conditions
              x  y     z
         0 -0.6  3  29.0
         1  0.2  4  24.0
@@ -124,17 +124,17 @@ def random_pool_on_state(
         3  9.0  3  29.0
         4 -9.4  3  22.0
     """
-    return wrap_to_use_state(random_pool)(
+    return wrap_to_use_state(_result)(
         s, num_samples=num_samples, random_state=random_state, replace=replace, **kwargs
     )
 
 
-def random_pool(
+def _result(
     variables: VariableCollection,
-    num_samples: int = 5,
-    random_state: Optional[int] = None,
-    replace: bool = True,
-) -> Result:
+    num_samples: int,
+    random_state: Optional[int],
+    replace: bool,
+):
     """
     Create a sequence of conditions randomly sampled from independent variables.
 
@@ -146,6 +146,33 @@ def random_pool(
 
     Returns: a Result / Delta object with the conditions as a pd.DataFrame in the `conditions` field
 
+    """
+    conditions = _base(
+        variables=variables,
+        num_samples=num_samples,
+        random_state=random_state,
+        replace=replace,
+    )
+    return Result(conditions=conditions)
+
+
+def _base(
+    variables: VariableCollection,
+    num_samples: int = 5,
+    random_state: Optional[int] = None,
+    replace: bool = True,
+) -> pd.DataFrame:
+    """
+    Create a sequence of conditions randomly sampled from independent variables.
+
+    Args:
+        variables: the description of all the variables in the AER experiment.
+        num_samples: the number of conditions to produce
+        random_state: the seed value for the random number generator
+        replace: if True, allow repeated values
+
+    Returns: the generated conditions as a dataframe
+
     Examples:
         >>> from autora.state.delta import State
         >>> from autora.variable import VariableCollection, Variable
@@ -155,10 +182,10 @@ def random_pool(
 
         With one independent variable "x", and some allowed_values we get some of those values
         back when running the experimentalist:
-        >>> random_pool(
+        >>> _base(
         ...     VariableCollection(
         ...         independent_variables=[Variable(name="x", allowed_values=range(10))
-        ... ]), random_state=1)["conditions"]
+        ... ]), random_state=1)
            x
         0  4
         1  5
@@ -169,10 +196,10 @@ def random_pool(
 
         ... with one independent variable "x", and a value_range,
         we get a sample of the range back when running the experimentalist:
-        >>> random_pool(
+        >>> _base(
         ...     VariableCollection(independent_variables=[
         ...         Variable(name="x", value_range=(-5, 5))
-        ... ]), random_state=1)["conditions"]
+        ... ]), random_state=1)
                   x
         0  0.118216
         1  4.504637
@@ -183,16 +210,16 @@ def random_pool(
 
 
         The allowed_values or value_range must be specified:
-        >>> random_pool(VariableCollection(independent_variables=[Variable(name="x")]))
+        >>> _base(VariableCollection(independent_variables=[Variable(name="x")]))
         Traceback (most recent call last):
         ...
         ValueError: allowed_values or [value_range and type==REAL] needs to be set...
 
         With two independent variables, we get independent samples on both axes:
-        >>> random_pool(VariableCollection(independent_variables=[
+        >>> _base(VariableCollection(independent_variables=[
         ...         Variable(name="x1", allowed_values=range(1, 5)),
         ...         Variable(name="x2", allowed_values=range(1, 500)),
-        ... ]), num_samples=10, replace=True, random_state=1)["conditions"]
+        ... ]), num_samples=10, replace=True, random_state=1)
            x1   x2
         0   2  434
         1   3  212
@@ -206,7 +233,7 @@ def random_pool(
         9   2   14
 
         If any of the variables have unspecified allowed_values, we get an error:
-        >>> random_pool(
+        >>> _base(
         ...     VariableCollection(independent_variables=[
         ...         Variable(name="x1", allowed_values=[1, 2]),
         ...         Variable(name="x2"),
@@ -218,12 +245,12 @@ def random_pool(
 
         We can specify arrays of allowed values:
 
-        >>> random_pool(
+        >>> _base(
         ...     VariableCollection(independent_variables=[
         ...         Variable(name="x", allowed_values=np.linspace(-10, 10, 101)),
         ...         Variable(name="y", allowed_values=[3, 4]),
         ...         Variable(name="z", allowed_values=np.linspace(20, 30, 11)),
-        ... ]), random_state=1)["conditions"]
+        ... ]), random_state=1)
              x  y     z
         0 -0.6  3  29.0
         1  0.2  4  24.0
@@ -250,65 +277,15 @@ def random_pool(
                 "%s" % (iv)
             )
 
-    conditions = pd.DataFrame(raw_conditions)
-    return Result(conditions=conditions)
+    return pd.DataFrame(raw_conditions)
 
 
-def random_sample_on_state(s: State, **kwargs) -> State:
-    """
-    Take a random sample from some input conditions.
-
-    Args:
-        s: a State object with a `variables` field.
-
-    Returns: a State object updated with the new conditions
-
-    Examples:
-        >>> from autora.state.bundled import StandardState
-        >>> s = StandardState(conditions=pd.DataFrame({"x": range(100, 200)}))
-        >>> random_sample_on_state(s, random_state=1, replace=False, num_samples=3).conditions
-              x
-        80  180
-        84  184
-        33  133
-
-    """
-    return wrap_to_use_state(random_sample)(s, **kwargs)
+# Option 2:
+on_state = _state
+to_result = _result
+raw = _base
 
 
-def random_sample(
-    conditions: Union[pd.DataFrame, np.ndarray, np.recarray],
-    num_samples: int = 1,
-    random_state: Optional[int] = None,
-    replace: bool = False,
-) -> Result:
-    """
-    Take a random sample from some input conditions.
-
-    Args:
-        conditions: the conditions to sample from
-        num_samples: the number of conditions to produce
-        random_state: the seed value for the random number generator
-        replace: if True, allow repeated values
-
-    Returns: a Result object with a field `conditions` containing a DataFrame of the sampled
-    conditions
-
-    Examples:
-        From a pd.DataFrame:
-        >>> import pandas as pd
-        >>> random_sample(
-        ...     pd.DataFrame({"x": range(100, 200)}), num_samples=5, random_state=180)["conditions"]
-              x
-        67  167
-        71  171
-        64  164
-        63  163
-        96  196
-
-    """
-    return Result(
-        conditions=pd.DataFrame.sample(
-            conditions, random_state=random_state, n=num_samples, replace=replace
-        )
-    )
+# Option 3:
+run = _state
+run_on_state = _state

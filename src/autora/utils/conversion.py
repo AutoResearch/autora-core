07f05fe8add3baae81dd3fb38e0e9bc3b0a080a6
Sebastian Musslick <sebastian@musslick.de>
2023-08-18 12:32:17 -0400

    added function to align df with metadata

--- src/autora/utils/conversion.py
+++ src/autora/utils/conversion.py
@@ -1,10 +1,12 @@
+from typing import List
+
 import pandas as pd
 
-from autora.variable import VariableCollection
+from autora.variable import IV
 
 
-def align_dataframe_to_metadata(
-    dataframe: pd.DataFrame, metadata: VariableCollection
+def align_dataframe_to_ivs(
+    dataframe: pd.DataFrame, independent_variables: List[IV]
 ) -> pd.DataFrame:
     """
     Aligns a dataframe to a metadata object, ensuring that the columns are in the same order
@@ -12,12 +14,12 @@ def align_dataframe_to_metadata(
 
     Args:
         dataframe: a dataframe with columns to align
-        metadata: a VariableCollection with independent variables
+        independent_variables: a list of independent variables
 
     Returns:
         a dataframe with columns in the same order as the independent variables in the metadata
     """
     variable_names = list()
-    for variable in metadata.independent_variables:
+    for variable in independent_variables:
         variable_names.append(variable.name)
     return dataframe[variable_names]

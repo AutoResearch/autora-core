febaa64503743e0b0ce975a87bb67496bbd39f3c
John Gerrard Holland <john_holland1@brown.edu>
2023-08-25 10:36:57 +0200

    feat: accept empty arguments for input and output paths

--- src/autora/workflow/__main__.py
+++ src/autora/workflow/__main__.py
@@ -1,6 +1,7 @@
 import importlib
 import logging
 import pathlib
+from typing import Optional, Union
 
 import dill
 import typer
@@ -16,31 +17,22 @@ def main(
         str, typer.Argument(help="Function to load")
     ],
     input_path: Annotated[
-        pathlib.Path, typer.Argument(help="Path to a .dill file with the initial state")
-    ],
+        Optional[pathlib.Path],
+        typer.Option(help="Path to a .dill file with the initial state"),
+    ] = None,
     output_path: Annotated[
-        pathlib.Path,
-        typer.Argument(help="Path to output the final state as a .dill file"),
-    ],
+        Optional[pathlib.Path],
+        typer.Option(help="Path to output the final state as a .dill file"),
+    ] = None,
     verbose: Annotated[bool, typer.Option(help="Turns on info logging level.")] = False,
     debug: Annotated[bool, typer.Option(help="Turns on debug logging level.")] = False,
 ):
-    _logger.info("Initializing")
     _configure_logger(debug, verbose)
-
     starting_state = _load_state(input_path)
     _logger.info(f"Starting State: {starting_state}")
-
-    module_name, function_name = fully_qualified_function_name.rsplit(".", 1)
-
-    module = importlib.import_module(module_name)
-    function = getattr(module, function_name)
-
+    function = _load_function(fully_qualified_function_name)
     ending_state = function(starting_state)
-
     _logger.info(f"Ending State: {ending_state}")
-
-    _logger.info("Writing out results")
     _dump_state(ending_state, output_path)
 
     return
@@ -55,17 +47,35 @@ def _configure_logger(debug, verbose):
         _logger.info("using INFO logging level")
 
 
-def _load_state(path: pathlib.Path) -> State:
-    _logger.debug(f"_load_state: loading from {path=}")
-    with open(path, "rb") as f:
-        state_ = dill.load(f)
+def _load_state(path: Optional[pathlib.Path]) -> Union[State, None]:
+    if path is not None:
+        _logger.debug(f"_load_state: loading from {path=}")
+        with open(path, "rb") as f:
+            state_ = dill.load(f)
+    else:
+        _logger.debug(f"_load_state: {path=} -> returning None")
+        state_ = None
     return state_
 
 
-def _dump_state(state_: State, path: pathlib.Path) -> None:
-    _logger.debug(f"_dump_state: dumping to {path=}")
-    with open(path, "wb") as f:
-        dill.dump(state_, f)
+def _load_function(fully_qualified_function_name: str):
+    _logger.debug(f"_load_function: Loading function {fully_qualified_function_name}")
+    module_name, function_name = fully_qualified_function_name.rsplit(".", 1)
+    module = importlib.import_module(module_name)
+    function = getattr(module, function_name)
+    _logger.debug(f"_load_function: Loaded function {function} from {module}")
+    return function
+
+
+def _dump_state(state_: State, path: Optional[pathlib.Path]) -> None:
+    if path is not None:
+        _logger.debug(f"_dump_state: dumping to {path=}")
+        with open(path, "wb") as f:
+            dill.dump(state_, f)
+    else:
+        _logger.debug(f"_dump_state: {path=} so writing to stdout")
+        print(dill.dumps(state_))
+    return
 
 
 if __name__ == "__main__":

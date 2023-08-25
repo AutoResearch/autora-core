a643c791d3444d1b06597489c4a44d345ac67745
John Gerrard Holland <john_holland1@brown.edu>
2023-08-25 19:33:32 +0200

    test: add test of a StandardState object

--- tests/test_load_dump_state.py
+++ tests/test_load_dump_state.py
@@ -4,10 +4,13 @@ import tempfile
 from hypothesis import Verbosity, given, settings
 from hypothesis import strategies as st
 
+from autora.state import StandardState
 from autora.workflow.__main__ import _dump_state, _load_state
 
 
-@given(st.text())
+@given(
+    st.builds(StandardState, st.text(), st.text(), st.text(), st.lists(st.integers()))
+)
 @settings(verbosity=Verbosity.verbose)
 def test_load_inverts_dump(s):
     with tempfile.TemporaryDirectory() as dir:

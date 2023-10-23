69159ab2acb03ff79366c5ce20ee674a93a4d764
John Gerrard Holland <john_holland1@brown.edu>
2023-10-23 10:45:35 -0400

    feat: add ability to choose between dill, pickle and yaml loaders

--- src/autora/workflow/__main__.py
+++ src/autora/workflow/__main__.py
@@ -1,15 +1,34 @@
 import importlib
 import logging
 import pathlib
-from typing import Optional, Union
+from collections import namedtuple
+from enum import Enum
+from typing import Callable, Dict, Literal, Optional, Tuple, Union
 
-import dill
 import typer
 from typing_extensions import Annotated
 
 from autora.state import State
 
 _logger = logging.getLogger(__name__)
+
+
+SerializerDef = namedtuple(
+    "SerializerDef", ["module", "load", "dump", "dumps", "file_mode"]
+)
+
+
+serializer_dict: Dict[str, SerializerDef] = dict(
+    pickle=SerializerDef("pickle", "load", "dump", "dumps", "b"),
+    yaml=SerializerDef("autora.workflow.serializer.yaml_", "load", "dump", "dumps", ""),
+    dill=SerializerDef("dill", "load", "dump", "dumps", "b"),
+)
+
+
+class _Serializer(str, Enum):
+    dill = "dill"
+    pickle = "pickle"
+    yaml = "yaml"
 
 
 def main(
@@ -24,16 +43,28 @@ def main(
         Optional[pathlib.Path],
         typer.Option(help="Path to output the final state as a .dill file"),
     ] = None,
+    loader: Annotated[
+        _Serializer,
+        typer.Option(
+            help="deserializer to use to load the data",
+        ),
+    ] = _Serializer.dill,
+    dumper: Annotated[
+        _Serializer,
+        typer.Option(
+            help="serializer to use to save the data",
+        ),
+    ] = _Serializer.dill,
     verbose: Annotated[bool, typer.Option(help="Turns on info logging level.")] = False,
     debug: Annotated[bool, typer.Option(help="Turns on debug logging level.")] = False,
 ):
     _configure_logger(debug, verbose)
-    starting_state = _load_state(in_path)
+    starting_state = _load_state(in_path, loader)
     _logger.info(f"Starting State: {starting_state}")
     function = _load_function(fully_qualified_function_name)
     ending_state = function(starting_state)
     _logger.info(f"Ending State: {ending_state}")
-    _dump_state(ending_state, out_path)
+    _dump_state(ending_state, out_path, dumper)
 
     return
 
@@ -47,11 +78,29 @@ def _configure_logger(debug, verbose):
         _logger.info("using INFO logging level")
 
 
-def _load_state(path: Optional[pathlib.Path]) -> Union[State, None]:
+def _get_serializer_mode(
+    serializer: _Serializer, interface: Literal["load", "dump", "dumps"]
+) -> Tuple[Callable, str]:
+    serializer_def = serializer_dict[serializer]
+    module = serializer_def.module
+    interface_function_name = getattr(serializer_def, interface)
+    _logger.debug(
+        f"_get_serializer: loading {interface_function_name=} from" f" {module=}"
+    )
+    module = importlib.import_module(module)
+    function = getattr(module, interface_function_name)
+    file_mode = serializer_def.file_mode
+    return function, file_mode
+
+
+def _load_state(
+    path: Optional[pathlib.Path], loader: _Serializer = _Serializer.dill
+) -> Union[State, None]:
     if path is not None:
+        load, file_mode = _get_serializer_mode(loader, "load")
         _logger.debug(f"_load_state: loading from {path=}")
-        with open(path, "rb") as f:
-            state_ = dill.load(f)
+        with open(path, f"r{file_mode}") as f:
+            state_ = load(f)
     else:
         _logger.debug(f"_load_state: {path=} -> returning None")
         state_ = None
@@ -67,15 +116,21 @@ def _load_function(fully_qualified_function_name: str):
     return function
 
 
-def _dump_state(state_: State, path: Optional[pathlib.Path]) -> None:
+def _dump_state(
+    state_: State,
+    path: Optional[pathlib.Path],
+    dumper: _Serializer = _Serializer.dill,
+) -> None:
     if path is not None:
+        dump, file_mode = _get_serializer_mode(dumper, "dump")
         _logger.debug(f"_dump_state: dumping to {path=}")
         path.parent.mkdir(parents=True, exist_ok=True)
-        with open(path, "wb") as f:
-            dill.dump(state_, f)
+        with open(path, f"w{file_mode}") as f:
+            dump(state_, f)
     else:
+        dumps, _ = _get_serializer_mode(dumper, "dumps")
         _logger.debug(f"_dump_state: {path=} so writing to stdout")
-        print(dill.dumps(state_))
+        print(dumps(state_))
     return
 
 
--- tests/test_load_dump_state.py
+++ tests/test_load_dump_state.py
@@ -1,20 +1,23 @@
 import pathlib
 import tempfile
+import uuid
 
 from hypothesis import Verbosity, given, settings
 from hypothesis import strategies as st
 
 from autora.state import StandardState
-from autora.workflow.__main__ import _dump_state, _load_state
+from autora.workflow.__main__ import _dump_state, _load_state, _Serializer
 
 
 @given(
-    st.builds(StandardState, st.text(), st.text(), st.text(), st.lists(st.integers()))
+    st.builds(StandardState, st.text(), st.text(), st.text(), st.lists(st.integers())),
+    st.sampled_from(_Serializer),
 )
 @settings(verbosity=Verbosity.verbose)
-def test_load_inverts_dump(s):
+def test_load_inverts_dump(s, serializer):
     with tempfile.TemporaryDirectory() as dir:
-        path = pathlib.Path(dir, "x.dill")
+        path = pathlib.Path(dir, f"{str(uuid.uuid4())}.{serializer}")
         print(path, s)
-        _dump_state(s, path)
-        assert _load_state(path) == s
+
+        _dump_state(s, path, dumper=serializer)
+        assert _load_state(path, loader=serializer) == s

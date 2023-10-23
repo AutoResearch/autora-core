bca7a0f67528f4520a91d31720c1dd35b84fb6d8
John Gerrard Holland <john_holland1@brown.edu>
2023-10-23 12:42:23 -0400

    refactor: change variable names for loader and dumper to be clearer

--- src/autora/workflow/__main__.py
+++ src/autora/workflow/__main__.py
@@ -21,17 +21,17 @@ def main(
         Optional[pathlib.Path],
         typer.Option(help="Path to a .dill file with the initial state"),
     ] = None,
-    out_path: Annotated[
-        Optional[pathlib.Path],
-        typer.Option(help="Path to output the final state as a .dill file"),
-    ] = None,
-    loader: Annotated[
+    in_loader: Annotated[
         SerializersSupported,
         typer.Option(
             help="deserializer to use to load the data",
         ),
     ] = SerializersSupported.dill,
-    dumper: Annotated[
+    out_path: Annotated[
+        Optional[pathlib.Path],
+        typer.Option(help="Path to output the final state as a .dill file"),
+    ] = None,
+    out_dumper: Annotated[
         SerializersSupported,
         typer.Option(
             help="serializer to use to save the data",
@@ -41,12 +41,12 @@ def main(
     debug: Annotated[bool, typer.Option(help="Turns on debug logging level.")] = False,
 ):
     _configure_logger(debug, verbose)
-    starting_state = _load_state(in_path, loader)
+    starting_state = _load_state(in_path, in_loader)
     _logger.info(f"Starting State: {starting_state}")
     function = _load_function(fully_qualified_function_name)
     ending_state = function(starting_state)
     _logger.info(f"Ending State: {ending_state}")
-    _dump_state(ending_state, out_path, dumper)
+    _dump_state(ending_state, out_path, out_dumper)
 
     return
 

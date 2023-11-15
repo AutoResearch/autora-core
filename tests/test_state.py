dd02c496e85a3c59ad9fafbe70201911fa0c7b1d
John Gerrard Holland <john_holland1@brown.edu>
2023-11-15 12:03:05 -0500

    test: split main logic for making dataframes into a series and dataframe generator

--- tests/test_state.py
+++ tests/test_state.py
@@ -1,12 +1,15 @@
+import logging
 import pickle
-from typing import Sequence
+from typing import Optional, Sequence
 
 import pandas as pd
-from hypothesis import given, settings
+from hypothesis import given
 from hypothesis import strategies as st
 
 from autora.state import StandardStateDataClass
 from autora.variable import ValueType, Variable, VariableCollection
+
+logger = logging.getLogger(__name__)
 
 
 @st.composite
@@ -67,24 +70,58 @@ def variablecollection_strategy(draw):
 
 
 @st.composite
-def dataframe_strategy(draw, variables: Sequence[Variable]):
-    n_entries = draw(st.integers(min_value=0))
+def data_length_strategy(draw, max_value=10_000):
+    return draw(st.integers(min_value=0, max_value=max_value))
+
+
+@st.composite
+def series_strategy(
+    draw, variable: Optional[Variable] = None, n_entries: Optional[int] = None
+):
+    if variable is None:
+        variable = draw(variable_strategy())
+    if n_entries is None:
+        n_entries = draw(data_length_strategy())
+
+    logger.debug(f"{variable} {n_entries}")
+
+    if variable.allowed_values is not None:
+        value_strategy = st.sampled_from(variable.allowed_values)
+    elif variable.value_range is not None:
+        min_value, max_value = sorted(variable.value_range)
+        if isinstance(min_value, int):
+            value_strategy = st.integers(min_value=min_value, max_value=max_value)
+        elif isinstance(min_value, float):
+            value_strategy = st.floats(min_value=min_value, max_value=max_value)
+        else:
+            raise NotImplementedError(
+                f"value_strategy for {variable.value_range} not implemented"
+            )
+    else:
+        value_strategy = st.floats()
+    value = draw(st.lists(value_strategy, min_size=n_entries, max_size=n_entries))
+    result = pd.Series(value, name=variable.name)
+    logger.debug(result)
+    return result
+
+
+@st.composite
+def dataframe_strategy(
+    draw,
+    variables: Optional[Sequence[Variable]] = None,
+    n_entries: Optional[int] = None,
+):
+    if variables is None:
+        variables = draw(st.lists(variable_strategy()))
+    if n_entries is None:
+        n_entries = draw(data_length_strategy())
+
     d = {}
     for v in variables:
-        if v.allowed_values is not None:
-            value_strategy = st.sampled_from(v.allowed_values)
-        elif v.value_range is not None:
-            min_value, max_value = sorted(v.value_range)
-            if isinstance(min_value, int):
-                value_strategy = st.integers(min_value=min_value, max_value=max_value)
-            elif isinstance(min_value, float):
-                value_strategy = st.floats(min_value=min_value, max_value=max_value)
-        else:
-            value_strategy = st.floats()
-        d[v.name] = draw(
-            st.lists(value_strategy, min_size=n_entries, max_size=n_entries)
-        )
+        series = draw(series_strategy(variable=v, n_entries=n_entries))
+        d[v.name] = series
     df = pd.DataFrame(d)
+    logger.debug(df)
     return df
 
 
@@ -117,11 +154,16 @@ def test_core_dataclasses_serialize_deserialize(o):
     assert o_loaded == o
 
 
-@settings(print_blob=True)
+@given(st.one_of(series_strategy(), dataframe_strategy()))
+def test_core_dataframe_serialize_deserialize(o):
+    o_loaded = pickle.loads(pickle.dumps(o))
+    pd.DataFrame.equals(o, o_loaded)
+
+
 @given(standard_state_dataclass_strategy())
 def test_standard_state_dataclass_serialize_deserialize(o: StandardStateDataClass):
     o_dumped = pickle.dumps(o)
-    o_loaded: StandardStateDataClass = pickle.loads(o_dumped)
+    o_loaded = pickle.loads(o_dumped)
 
     assert o.variables == o_loaded.variables
     assert pd.DataFrame.equals(o.conditions, o_loaded.conditions)

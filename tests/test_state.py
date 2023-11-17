097cfa4581ee9f18b1e9caa9996d2d359d28668d
John Gerrard Holland <john_holland1@brown.edu>
2023-11-17 14:36:29 -0500

    test: remove tests of serializing and deserializing models and dataframes – handled by their packages

--- tests/test_state.py
+++ tests/test_state.py
@@ -2,44 +2,13 @@ import logging
 import pickle
 
 import pandas as pd
-from hypothesis import given, settings
-from hypothesis import strategies as st
+from hypothesis import given
 
 from autora.state import StandardStateDataClass
 
-from .strategies import (
-    dataframe_strategy,
-    model_strategy,
-    series_strategy,
-    standard_state_dataclass_strategy,
-)
+from .strategies import standard_state_dataclass_strategy
 
 logger = logging.getLogger(__name__)
-
-
-@given(st.one_of(series_strategy(), dataframe_strategy()))
-def test_core_dataframe_serialize_deserialize(o):
-    o_loaded = pickle.loads(pickle.dumps(o))
-    assert pd.DataFrame.equals(o, o_loaded)
-
-
-@settings(max_examples=10)
-@given(
-    model_strategy(
-        value_strategy=st.sampled_from(
-            [
-                st.integers(),
-                st.floats(
-                    min_value=0, max_value=1, allow_nan=False, allow_subnormal=False
-                ),
-                st.floats(allow_infinity=False, allow_nan=False, allow_subnormal=False),
-            ]
-        )
-    )
-)
-def test_model_serialize_deserialize(o):
-    o_loaded = pickle.loads(pickle.dumps(o))
-    assert o == o_loaded
 
 
 @given(standard_state_dataclass_strategy())

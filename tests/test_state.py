bafa59947110cef7716c4854cfabff8a67355632
John Gerrard Holland <john_holland1@brown.edu>
2023-11-17 17:42:32 -0500

    test: update test_state to use new dump_load function

--- tests/test_state.py
+++ tests/test_state.py
@@ -1,24 +1,19 @@
 import logging
 
 import pandas as pd
-from hypothesis import given, settings
+from hypothesis import given
 
 from autora.state import StandardStateDataClass
 
 from .strategies import standard_state_dataclass_strategy
-from .test_serializer import SUPPORTED_SERIALIZERS
+from .test_serializer import serializer_dump_load_strategy
 
 logger = logging.getLogger(__name__)
 
 
-@settings(max_examples=1000)
-@given(standard_state_dataclass_strategy(), SUPPORTED_SERIALIZERS)
-def test_standard_state_dataclass_serialize_deserialize(
-    o: StandardStateDataClass, loads_dumps
-):
-    loads, dumps = loads_dumps
-    o_loaded = loads(dumps(o))
-    print(o)
+@given(standard_state_dataclass_strategy(), serializer_dump_load_strategy())
+def test_state_serialize_deserialize(o: StandardStateDataClass, dump_load):
+    o_loaded = dump_load(o)
     assert o.variables == o_loaded.variables
     assert pd.DataFrame.equals(o.conditions, o_loaded.conditions)
     assert pd.DataFrame.equals(o.experiment_data, o_loaded.experiment_data)

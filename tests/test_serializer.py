85eebf40bc8b0ae616cf8d2b71dc1c70557b2929
John Gerrard Holland <john_holland1@brown.edu>
2023-11-20 10:15:47 -0500

    test: extend dumps-reloads strategy to save and load to disk as well

--- tests/test_serializer.py
+++ tests/test_serializer.py
@@ -1,5 +1,8 @@
 import importlib
 import logging
+import pathlib
+import tempfile
+import uuid
 
 import hypothesis.strategies as st
 
@@ -27,7 +30,51 @@ def serializer_loads_dumps_strategy(draw):
 
 
 @st.composite
+def serializer_dump_load_string_strategy(draw):
+    """Strategy returns a function which dumps an object and reloads it via a bytestream."""
+    module_ = draw(AVAILABLE_SERIALIZERS)
+    loads, dumps = module_.loads, module_.dumps
+
+    def _load_dump_via_string(o):
+        print(f"load dump via string using {module_=}")
+        return loads(dumps(o))
+
+    return _load_dump_via_string
+
+
+@st.composite
+def serializer_dump_load_binary_file_strategy(draw):
+    """Strategy returns a function which dumps an object reloads it via a temporary binary file."""
+    module_ = draw(AVAILABLE_SERIALIZERS)
+    load, dump = module_.load, module_.dump
+
+    def _load_dump_via_disk(o):
+        print(f"load dump via disk using {module_=}")
+        with tempfile.TemporaryDirectory() as tempdir:
+            filename = str(uuid.uuid1())
+            with open(pathlib.Path(tempdir, filename), "wb") as f:
+                dump(o, f)
+            with open(pathlib.Path(tempdir, filename), "rb") as f:
+                o_loaded = load(f)
+        return o_loaded
+
+    return _load_dump_via_disk
+
+
+@st.composite
 def serializer_dump_load_strategy(draw):
-    module = draw(AVAILABLE_SERIALIZERS)
-    loads, dumps = module.loads, module.dumps
-    return lambda x: loads(dumps(x))
+    """Strategy returns a function which dumps an object and reloads it via a supported method."""
+    _dump_load = draw(
+        st.one_of(
+            serializer_dump_load_string_strategy(),
+            serializer_dump_load_binary_file_strategy(),
+        )
+    )
+    return _dump_load
+
+
+if __name__ == "__main__":
+    o = list("abcde")
+    loader_dumper_disk = serializer_dump_load_strategy().example()
+    o_loaded = loader_dumper_disk(o)
+    print(o, o_loaded)

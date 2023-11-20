64e8442e70352c63552bc9a61784ffed39013fcb
John Gerrard Holland <john_holland1@brown.edu>
2023-11-20 10:38:43 -0500

    test: add support for yaml serialization

--- tests/test_serializer.py
+++ tests/test_serializer.py
@@ -3,40 +3,46 @@ import logging
 import pathlib
 import tempfile
 import uuid
+from collections import namedtuple
 
 import hypothesis.strategies as st
 
 logger = logging.getLogger(__name__)
 
-_SUPPORTED_SERIALIZERS = ["pickle", "dill"]
+_SUPPORTED_SERIALIZERS = [
+    ("pickle", "b"),
+    ("dill", "b"),
+    ("autora.serializer.yaml_", ""),
+]
+_SERIALIZER_DEF = namedtuple("_SERIALIZER_DEF", ["name", "module", "file_type"])
 _AVAILABLE_SERIALIZERS = []
 
-for module_name in _SUPPORTED_SERIALIZERS:
+for module_name, file_type in _SUPPORTED_SERIALIZERS:
     try:
         module = importlib.import_module(module_name)
     except ImportError:
         logger.info(f"serializer {module} not available")
         continue
-    _AVAILABLE_SERIALIZERS.append(module)
+    _AVAILABLE_SERIALIZERS.append(_SERIALIZER_DEF(module_name, module, file_type))
 
 AVAILABLE_SERIALIZERS = st.sampled_from(_AVAILABLE_SERIALIZERS)
 
 
 @st.composite
 def serializer_loads_dumps_strategy(draw):
-    module = draw(AVAILABLE_SERIALIZERS)
-    loads, dumps = module.loads, module.dumps
+    serializer = draw(AVAILABLE_SERIALIZERS)
+    loads, dumps = serializer.module.loads, serializer.module.dumps
     return loads, dumps
 
 
 @st.composite
 def serializer_dump_load_string_strategy(draw):
     """Strategy returns a function which dumps an object and reloads it via a bytestream."""
-    module_ = draw(AVAILABLE_SERIALIZERS)
-    loads, dumps = module_.loads, module_.dumps
+    serializer = draw(AVAILABLE_SERIALIZERS)
+    loads, dumps = serializer.module.loads, serializer.module.dumps
 
     def _load_dump_via_string(o):
-        print(f"load dump via string using {module_=}")
+        print(f"load dump via string using {serializer.module=}")
         return loads(dumps(o))
 
     return _load_dump_via_string
@@ -45,16 +51,16 @@ def serializer_dump_load_string_strategy(draw):
 @st.composite
 def serializer_dump_load_binary_file_strategy(draw):
     """Strategy returns a function which dumps an object reloads it via a temporary binary file."""
-    module_ = draw(AVAILABLE_SERIALIZERS)
-    load, dump = module_.load, module_.dump
+    serializer = draw(AVAILABLE_SERIALIZERS)
+    load, dump = serializer.module.load, serializer.module.dump
 
     def _load_dump_via_disk(o):
-        print(f"load dump via disk using {module_=}")
+        print(f"load dump via disk using {serializer.module=}")
         with tempfile.TemporaryDirectory() as tempdir:
             filename = str(uuid.uuid1())
-            with open(pathlib.Path(tempdir, filename), "wb") as f:
+            with open(pathlib.Path(tempdir, filename), f"w{serializer.file_type}") as f:
                 dump(o, f)
-            with open(pathlib.Path(tempdir, filename), "rb") as f:
+            with open(pathlib.Path(tempdir, filename), f"r{serializer.file_type}") as f:
                 o_loaded = load(f)
         return o_loaded
 

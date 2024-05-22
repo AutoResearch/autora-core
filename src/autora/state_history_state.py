88425f2138d80dc461787e5032f3cfba686d71f9
John Gerrard Holland <john_holland1@brown.edu>
2024-05-22 11:50:38 -0400

    feat: add a new state with history which stores the full state object

---
+++ src/autora/state_history_state.py
@@ -0,0 +1,160 @@
+from dataclasses import dataclass, field, replace
+from typing import List, Set, Union
+
+from autora.state import State
+
+
+@dataclass(frozen=True)
+class StateHistory(State):
+    """
+    Base object for dataclasses which use the Delta mechanism.
+
+    Examples:
+        >>> from dataclasses import dataclass, field
+        >>> from typing import List, Optional
+        >>> from autora.state import Delta
+
+        We define a dataclass where each field (which is going to be delta-ed) has additional
+        metadata "delta" which describes its delta behaviour.
+        >>> @dataclass(frozen=True)
+        ... class ListState(StateHistory):
+        ...    l: List = field(default_factory=list, metadata={"delta": "extend"})
+        ...    m: List = field(default_factory=list, metadata={"delta": "replace"})
+
+        >>> ListState()
+        ListState(history=[], meta=set(), l=[], m=[])
+
+
+        Now we instantiate the dataclass...
+        >>> l = ListState(l=list("abc"), m=list("xyz"), meta="initial")
+        >>> l  # doctest: +NORMALIZE_WHITESPACE
+        ListState(history=[],
+                  meta={'initial'},
+                  l=['a', 'b', 'c'],
+                  m=['x', 'y', 'z'])
+
+        ... and can add deltas to it. `l` will be extended:
+        >>> l + Delta(l=list("def"), meta="first")  # doctest: +NORMALIZE_WHITESPACE +ELLIPSIS
+        ListState(
+            history=[ListState(history=[], meta={'initial'}, l=['a', 'b', 'c'], m=['x', 'y', 'z'])],
+            meta={'first'},
+            l=['a', 'b', 'c', 'd', 'e', 'f'],
+            m=['x', 'y', 'z'])
+        )
+
+        ... wheras `m` will be replaced:
+        >>> l + Delta(m=list("uvw"))  # doctest: +NORMALIZE_WHITESPACE +ELLIPSIS
+        ListState(history=[..., {'m': ['u', 'v', 'w']}],
+                  l=['a', 'b', 'c'],
+                  m=['u', 'v', 'w'])
+
+        ... they can be chained:
+        >>> l + Delta(l=list("d")) + Delta(m=list("u"))  # doctest: +NORMALIZE_WHITESPACE +ELLIPSIS
+        ListState(history=[..., {'l': ['d']}, {'m': ['u']}],
+                  l=['a', 'b', 'c', 'd'],
+                  m=['u'])
+
+        ... and we update multiple fields with one Delta:
+        >>> l + Delta(l=list("ghi"), m=list("rst"))  # doctest: +NORMALIZE_WHITESPACE +ELLIPSIS
+        ListState(history=[..., {'l': ['g', 'h', 'i'], 'm': ['r', 's', 't']}],
+                  l=['a', 'b', 'c', 'g', 'h', 'i'],
+                  m=['r', 's', 't'])
+
+        A non-existent field will be ignored but included in the history:
+        >>> l + Delta(o="not a field")  # doctest: +ELLIPSIS
+        ListState(history=[..., {'o': 'not a field'}], l=['a', 'b', 'c'], m=['x', 'y', 'z'])
+
+        We can also use the `.update` method to do the same thing:
+        >>> l.update(l=list("ghi"), m=list("rst"))  # doctest: +NORMALIZE_WHITESPACE +ELLIPSIS
+        ListState(history=[..., {'l': ['g', 'h', 'i'], 'm': ['r', 's', 't']}],
+                  l=['a', 'b', 'c', 'g', 'h', 'i'],
+                  m=['r', 's', 't'])
+
+        We can also define fields which `append` the last result:
+        >>> @dataclass(frozen=True)
+        ... class AppendState(StateHistory):
+        ...    n: List = field(default_factory=list, metadata={"delta": "append"})
+
+        >>> m = AppendState(n=list("ɑβɣ"))
+        >>> m
+        AppendState(history=[{'n': ['ɑ', 'β', 'ɣ']}], n=['ɑ', 'β', 'ɣ'])
+
+        `n` will be appended:
+        >>> m + Delta(n="∂")  # doctest: +ELLIPSIS
+        AppendState(history=[..., {'n': '∂'}], n=['ɑ', 'β', 'ɣ', '∂'])
+
+        The metadata key "converter" is used to coerce types (inspired by
+        [PEP 712](https://peps.python.org/pep-0712/)):
+        >>> @dataclass(frozen=True)
+        ... class CoerceStateList(StateHistory):
+        ...    o: Optional[List] = field(default=None, metadata={"delta": "replace"})
+        ...    p: List = field(default_factory=list, metadata={"delta": "replace",
+        ...                                                    "converter": list})
+
+        >>> r = CoerceStateList()
+
+        If there is no `metadata["converter"]` set for a field, no coercion occurs
+        >>> r + Delta(o="not a list")
+        CoerceStateList(history=[{'o': None, 'p': []}, {'o': 'not a list'}], o='not a list', p=[])
+
+        If there is a `metadata["converter"]` set for a field, the data are coerced:
+        >>> r + Delta(p="not a list")  # doctest: +NORMALIZE_WHITESPACE
+        CoerceStateList(history=[{'o': None, 'p': []}, {'p': 'not a list'}],
+                        o=None, p=['n', 'o', 't', ' ', 'a', ' ', 'l', 'i', 's', 't'])
+
+        If the input data are of the correct type, they are returned unaltered:
+        >>> r + Delta(p=["a", "list"])  # doctest: +ELLIPSIS
+        CoerceStateList(history=[..., {'p': ['a', 'list']}], o=None, p=['a', 'list'])
+
+        With a converter, inputs are converted to the type output by the converter:
+        >>> import pandas as pd
+        >>> @dataclass(frozen=True)
+        ... class CoerceStateDataFrame(StateHistory):
+        ...    q: pd.DataFrame = field(default_factory=pd.DataFrame,
+        ...                            metadata={"delta": "replace",
+        ...                                      "converter": pd.DataFrame})
+
+        If the type is already correct, the object is passed to the converter,
+        but should be returned unchanged:
+        >>> s = CoerceStateDataFrame()
+        >>> (s + Delta(q=pd.DataFrame([("a",1,"alpha"), ("b",2,"beta")], columns=list("xyz")))).q
+           x  y      z
+        0  a  1  alpha
+        1  b  2   beta
+
+        If the type is not correct, the object is converted if possible. For a dataframe,
+        we can convert records:
+        >>> (s + Delta(q=[("a",1,"alpha"), ("b",2,"beta")])).q
+           0  1      2
+        0  a  1  alpha
+        1  b  2   beta
+
+        ... but be aware that the history stores the Delta object as it was provided,
+        before coercion.
+        >>> (s + Delta(q=[("a",1,"alpha"), ("b",2,"beta")])).history  # doctest: +ELLIPSIS
+        [..., {'q': [('a', 1, 'alpha'), ('b', 2, 'beta')]}]
+
+    """
+
+    history: List["StateHistory"] = field(default_factory=list)
+    meta: Set[str] = field(default_factory=set, metadata={"delta": "replace"})
+
+    def __add__(self, other):
+        new = super().__add__(other, warn_on_unused_fields=False)
+        new = replace(new, history=self.history + [self])
+        return new
+
+    def __post_init__(self):
+        if isinstance(self.meta, set):
+            pass
+        elif isinstance(self.meta, str):
+            object.__setattr__(self, "meta", set([self.meta]))
+        elif isinstance(self.meta, Union[list, tuple]):
+            object.__setattr__(self, "meta", set(self.meta))
+
+    def history_filter(self, *meta: str):
+        relevant_history_entries = filter(
+            lambda e: set(meta) <= e.meta,
+            self.history,
+        )
+        return relevant_history_entries
